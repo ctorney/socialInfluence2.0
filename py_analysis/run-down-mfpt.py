463ac438ca6e243f6935fb01a50963ab80bf2ed3
ctorney <colin.j.torney@gmail.com>
2014-11-29 17:22:30 +0000

    not quite working - next try a simple version with K=2 binomial samples

--- py_analysis/run-down-mfpt.py
+++ py_analysis/run-down-mfpt.py
@@ -25,15 +25,26 @@ def ETSWUP2(X):
     return sum(sp.binomial(Ns,j) * X**j* (1-X)**(Ns-j)* tswup(j/float(Ns))**2 for j in xrange(0,Ns+1))
 def VARTSWUP(X):  
     return sum(sp.binomial(Ns,j) * X**j* (1-X)**(Ns-j)* tswup(j/float(Ns))**2 for j in xrange(0,Ns+1))    - ETSWUP(X)**2
-def tup(X):  
+def tup(X):
+    #return 0.5
     return (1-X)*(ETSWUP(X)-(VARTSWUP(X)/(1.0-ETSWUP(X))))    
 def tdown(X): 
+    #return 0.5
     return (X)*(1.0-ETSWUP(X)-(VARTSWUP(X)/(ETSWUP(X))))   #(X)*(ETSWUP(X)-ETSWUP2(X))/(ETSWUP(X))
 def detODE(X):
     return numpy.array([sp.N(tup(X)-tdown(X))],dtype=float)
 def phi_int(y): 
     return np.prod([tdown(float(zz)/NA)/tup(float(zz)/NA) for zz in range(1,y+1)])
+def diffdown(x):
+    h=0.001
+    return (tdown(x+0.5*h)-tdown(x-0.5*h))/h
 
+def diffup(x):
+    h=0.000001
+    return (tup(x+0.5*h)-tup(x-0.5*h))/h
+    
+def diff2phi(x):
+    return -((1.0/tup(x))*diffup(x)-(1.0/tdown(x))*diffdown(x))
 def intexp2(x): 
     intexp = lambda y: 1.0/(tup(y)*phix(y))
     ph, err = integrate.quad(intexp,0,x)
@@ -41,35 +52,36 @@ def intexp2(x):
 
 def phi(x): 
     N21 = 0.5/int(NA)
-    intexp = lambda y: m.log(tup(y)/tdown(y))
+    intexp = lambda y: m.log(tdown(y)/tup(y))
     if x<N21:
         return 1
-    ph, err = integrate.quad(intexp,N21,x+N21)
-    return -ph
+    ph, err = integrate.quad(intexp,0,x)
+    return ph
     
 def phix(x): 
     N21 = 0.5/int(NA)
+    print x
     intexp = lambda y: m.log(tup(y)/tdown(y))
     if x<N21:
         return 1
     ph, err = integrate.quad(intexp,N21,x+N21)
     return m.exp(-ph*NA)
 
-numA = 21
+numA = 1
 alphas = np.zeros(numA)
 atimes = np.zeros(numA)
 atimes2 = np.zeros(numA)
 
 
 for acount in range(0,numA):
-    wg = 0.35 - 0.0125 * float(acount)
+    wg = 0.2# - 0.025 * float(acount)
     
     N2 = int(NA*0.5+1)
     
     Q = np.zeros((N2,N2))
 
     Q[0,0]=1.0-tup(0)-tdown(0)
-    Q[0,1]=tup(0)
+    Q[0,1]=tup(0)+tdown(0)
 
     Q[N2-1,N2-2]=tdown(N2/float(NA))
     Q[N2-1,N2-1]=1.0-tup(N2/float(NA))-tdown(N2/float(NA))
@@ -87,17 +99,22 @@ for acount in range(0,numA):
     intexp1 = lambda y: phix(y)*intexp2(y)
    
     atimes[acount] = times[0]
-    atimes2[acount] = sum([phix(i)*sum([1.0/(tup(float(z)/NA)*phix(z))  for z in range(0,i+1)])  for i in range(0,N2)] ) 
+    #atimes2[acount] = sum([phix(float(i)/NA)*sum([1.0/(tup(float(z)/NA)*phix(float(z)/NA))  for z in range(0,i+1)])  for i in range(0,N2)] ) 
     #atimes[acount] = sum([phix(float(i)/NA)*sum([1.0/(tup(float(z)/NA)*phi(z))  for z in range(0,i+1)])  for i in range(0,N2)] ) 
     #ph, err = integrate.quad(intexp1,0,0.5)
 #    atimes[acount] = ph
     x1=optimize.fsolve(detODE,0)[0]
-    x3=optimize.fsolve(detODE,0.5)[0]
+    shiftx=0.01
+    x3=optimize.fsolve(detODE,x1+shiftx)[0]
+    while (abs(x1-x3)<0.001):
+        x3=optimize.fsolve(detODE,x1+shiftx)[0]
+        shiftx=shiftx+0.01
     
-    dx=0.05
-    dx12 =(phi(x1+ dx)-2*phi(x1)+phi(x1-dx))/(dx**2);
-    dx32 =(phi(x3+ dx)-2*phi(x3)+phi(x3-dx))/(dx**2);
-    #atimes2[acount] = (tup(x1)**-1*(abs(dx12*dx32))**-0.5)*3.142*m.exp(NA*(phi(x3)-phi(x1)))
+   
+    dx12 =diff2phi(x1)
+    dx32 =diff2phi(x3)
+
+    atimes2[acount] = NA*(tup(x1)**-1*(abs(dx12*dx32))**-0.5)*3.142*m.exp(NA*(phi(x3)-phi(x1)))
 
     alphas[acount] = wg
 
